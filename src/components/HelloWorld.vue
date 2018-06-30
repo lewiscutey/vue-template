fe7cc3f44a2f92cf16bbe8cd90695378df348667
吕新 <949124363@qq.com>
2018-06-30 17:20:35 +0800

    pc

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App for PC !',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+  margin-top: 50px;
+}
+
+</style>
